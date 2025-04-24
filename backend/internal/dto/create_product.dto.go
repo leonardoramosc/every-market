128359fcb5c4a2b982f46e1fdc00f687ba928dfc
Leonardo Ramos <leorrc96@gmail.com>
2025-04-24 16:54:16 +0000

    create-product: validate input contains slice of images

--- backend/internal/dto/create_product.dto.go
+++ backend/internal/dto/create_product.dto.go
@@ -6,4 +6,5 @@ type CreateProductDto struct {
 	Price float64 `json:"price" binding:"required,numeric,gt=0"`
 	ImageURL string `json:"imageURL" binding:"required,url"`
 	CategoryID int `json:"categoryID" binding:"required"`
+	Images []string `json:"images" binding:"required,min=3,dive,required"`
 }
